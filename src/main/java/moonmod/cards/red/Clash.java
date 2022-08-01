b19501a2188d4c567b4be1d36022778bcf0f602e
Lucas <lucasfschraier@gmail.com>
2022-08-01 15:47:30 -0400

    feat: rework clash

---
+++ src/main/java/moonmod/cards/red/Clash.java
@@ -0,0 +1,58 @@
+package moonmod.cards.red;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.ClashEffect;
+
+import moonmod.cards.BaseCard;
+import moonmod.util.CardInfo;
+
+public class Clash extends BaseCard {
+
+    public static final String ID = "Clash";
+    public static final int COST = 1;
+    public static final int BASE_DMG = 9;
+    public static final int UPG_DMG = 3;
+
+    private final static CardInfo cardInfo = new CardInfo(
+        ID, 
+        COST, 
+        CardType.ATTACK, 
+        CardTarget.ENEMY, 
+        CardRarity.UNCOMMON, 
+        CardColor.RED
+    );
+  
+    public Clash() {
+        super(cardInfo);
+        this.setDamage(BASE_DMG, UPG_DMG);
+    }
+  
+  public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot((AbstractGameAction)new VFXAction((AbstractGameEffect)new ClashEffect(m.hb.cX, m.hb.cY), 0.1F)); 
+        addToBot((AbstractGameAction)new DamageAction((AbstractCreature)m, new DamageInfo((AbstractCreature)p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+  }
+
+    public void drawn() {
+        applyPowers();
+    }
+
+    public void applyPowers() {
+        int count = 0;
+        for (AbstractCard c : AbstractDungeon.player.hand.group) {
+            if (c.type != CardType.ATTACK) {
+                count++;
+            }
+        }
+        setCostForTurn(count);
+    }
+
+}
