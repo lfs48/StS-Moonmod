fffacb83528fcf0d1e2ad31bcaa452b39c402305
Lucas <lucasfschraier@gmail.com>
2022-08-02 18:31:22 -0400

    feat: lower juggernaut rarirty, buff upgrade

---
+++ src/main/java/moonmod/cards/red/Juggernaut.java
@@ -0,0 +1,39 @@
+package moonmod.cards.red;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.JuggernautPower;
+
+import moonmod.cards.BaseCard;
+import moonmod.util.CardInfo;
+
+public class Juggernaut extends BaseCard {
+
+    public static final String ID = "Juggernaut";
+    public static final int COST = 2;
+    public static final int BASE_MAGIC = 5;
+    public static final int UPG_MAGIC = 3;
+
+    private final static CardInfo cardInfo = new CardInfo(
+        ID, 
+        COST, 
+        CardType.POWER, 
+        CardTarget.SELF, 
+        CardRarity.UNCOMMON, 
+        CardColor.RED
+    );
+  
+    public Juggernaut() {
+        super(cardInfo);
+        this.setMagic(BASE_MAGIC, UPG_MAGIC);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new JuggernautPower((AbstractCreature)p, this.magicNumber), this.magicNumber));
+      }
+
+}
