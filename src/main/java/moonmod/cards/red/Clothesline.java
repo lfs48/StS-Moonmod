b9a2f98ca948c545ad350b46b4b26dc5b8e64834
Lucas <lucasfschraier@gmail.com>
2022-08-02 18:51:22 -0400

    feat: slightly buff clothesline

---
+++ src/main/java/moonmod/cards/red/Clothesline.java
@@ -0,0 +1,45 @@
+package moonmod.cards.red;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import moonmod.cards.BaseCard;
+import moonmod.util.CardInfo;
+
+public class Clothesline extends BaseCard {
+
+    public static final String ID = "Clothesline";
+    public static final int COST = 2;
+    public static final int BASE_DMG = 14;
+    public static final int UPG_DMG = 3;
+    public static final int BASE_MAGIC = 2;
+    public static final int UPG_MAGIC = 1;
+
+    private final static CardInfo cardInfo = new CardInfo(
+        ID, 
+        COST, 
+        CardType.ATTACK, 
+        CardTarget.ENEMY, 
+        CardRarity.COMMON, 
+        CardColor.RED
+    );
+  
+    public Clothesline() {
+        super(cardInfo);
+        this.setDamage(BASE_DMG, UPG_DMG);
+        this.setMagic(BASE_MAGIC, UPG_MAGIC);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot((AbstractGameAction)new DamageAction((AbstractCreature)m, new DamageInfo((AbstractCreature)p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)m, (AbstractCreature)p, (AbstractPower)new WeakPower((AbstractCreature)m, this.magicNumber, false), this.magicNumber));
+      }
+
+}
