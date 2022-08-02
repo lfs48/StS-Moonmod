a17330ea2cf8424481064831b1171ba941fd99db
Lucas <lucasfschraier@gmail.com>
2022-08-02 17:26:01 -0400

    feat: rework corruption to exhaust cards in hand for energy

--- src/main/java/moonmod/cards/red/Corruption.java
+++ src/main/java/moonmod/cards/red/Corruption.java
@@ -14,29 +14,29 @@ import com.megacrit.cardcrawl.vfx.BorderLongFlashEffect;
 import com.megacrit.cardcrawl.vfx.combat.VerticalAuraEffect;
 
 import moonmod.cards.BaseCard;
-import moonmod.powers.red.CorruptionStrPower;
+import moonmod.powers.red.CorruptionV2Power;
+import moonmod.powers.red.CorruptionV2UpgPower;
 import moonmod.util.CardInfo;
 
 public class Corruption extends BaseCard {
 
     public static final String ID = "Corruption";
-    public static final int COST = 2;
+    public static final int COST = 1;
 
-    public static final int STR_GAIN = 3;
-    public static final int HP_LOSS = 2;
+    public static final int BASE_MAGIC = 1;
 
     private final static CardInfo cardInfo = new CardInfo(
         ID, 
         COST, 
         CardType.POWER, 
-        CardTarget.NONE, 
+        CardTarget.SELF, 
         CardRarity.RARE, 
         CardColor.RED
     );
 
     public Corruption() {
         super(cardInfo);
-        this.setInnate(false, true);
+        this.setMagic(BASE_MAGIC);
     }
   
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -45,7 +45,9 @@ public class Corruption extends BaseCard {
         addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new VerticalAuraEffect(Color.PURPLE, p.hb.cX, p.hb.cY), 0.33F));
         addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new VerticalAuraEffect(Color.CYAN, p.hb.cX, p.hb.cY), 0.0F));
         addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new BorderLongFlashEffect(Color.MAGENTA), 0.0F, true));
-        addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new CorruptionStrPower((AbstractCreature)p, STR_GAIN, HP_LOSS), STR_GAIN));
-      }
+        if (this.upgraded) 
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new CorruptionV2UpgPower((AbstractCreature)p, this.magicNumber), this.magicNumber));
+        else
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new CorruptionV2Power((AbstractCreature)p, this.magicNumber), this.magicNumber));      }
 
 }
---
+++ src/main/java/moonmod/powers/red/CorruptionV2Power.java
@@ -0,0 +1,48 @@
+package moonmod.powers.red;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import moonmod.powers.BasePower;
+
+public class CorruptionV2Power extends BasePower {
+
+    public boolean upgraded;
+    public static final String POWER_ID = "CorruptionV2";
+    private static final AbstractPower.PowerType TYPE = AbstractPower.PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+
+    public CorruptionV2Power(AbstractCreature owner, int amount) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, amount);
+        this.updateDescription();
+    }
+  
+    public void updateDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(DESCRIPTIONS[0]);
+        sb.append(this.amount);
+        if (this.amount > 1) {
+            sb.append(DESCRIPTIONS[2]);
+            for (int i = 0; i < this.amount; i++)
+                sb.append("[R] "); 
+            sb.append(".");
+        } else {
+            sb.append(DESCRIPTIONS[1]);
+        }
+        this.description = sb.toString();
+      }
+  
+    public void atStartOfTurnPostDraw() {
+        flash();
+        addToBot((AbstractGameAction)new ExhaustAction(this.amount, true, false, false));
+        addToBot((AbstractGameAction)new GainEnergyAction(this.amount));
+    }
+
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+      }
+}
+  
