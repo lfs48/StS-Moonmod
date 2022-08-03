7867bf98e6c3f700213302660102839343c6ced8
Lucas <lucasfschraier@gmail.com>
2022-08-03 19:43:57 -0400

    feat: rework havoc

--- src/main/java/moonmod/cards/red/skill/Havoc.java
+++ src/main/java/moonmod/cards/red/skill/Havoc.java
@@ -2,8 +2,6 @@ package moonmod.cards.red.skill;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import moonmod.actions.HavocAction;
@@ -15,7 +13,6 @@ public class Havoc extends BaseCard {
     public static final String ID = "Havoc";
     public static final int COST = 1;
     public static final int UPG_COST = 0;
-    public static final int BASE_MAGIC = 2;
 
     private final static CardInfo cardInfo = new CardInfo(
         ID, 
@@ -28,14 +25,11 @@ public class Havoc extends BaseCard {
   
     public Havoc() {
         super(cardInfo);
-        this.setMagic(BASE_MAGIC);
         this.setCostUpgrade(UPG_COST);
     }
   
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot((AbstractGameAction)new HavocAction(
-            (AbstractCreature)(AbstractDungeon.getCurrRoom()).monsters.getRandomMonster(null, true, AbstractDungeon.cardRandomRng)
-        ));
+        addToBot((AbstractGameAction)new HavocAction());
     }
 
 }
