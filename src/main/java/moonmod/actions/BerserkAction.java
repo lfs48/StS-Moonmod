d77382d962915668be78a3af92304747bc312b9b
Lucas <lucasfschraier@gmail.com>
2022-08-05 17:41:06 -0400

    feat: add vfx to berserk

--- src/main/java/moonmod/actions/BerserkAction.java
+++ src/main/java/moonmod/actions/BerserkAction.java
@@ -1,6 +1,7 @@
 package moonmod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -8,6 +9,9 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
+import com.badlogic.gdx.graphics.Color;
 
 import moonmod.powers.red.BerserkPower;
 
@@ -21,6 +25,7 @@ public class BerserkAction extends AbstractGameAction {
   }
   
   public void update() {
+    addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new ShockWaveEffect(p.hb.cX, p.hb.cY, Color.RED, ShockWaveEffect.ShockWaveType.ADDITIVE), 0.5F));
     addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new StrengthPower((AbstractCreature)p, this.magicNumber), this.magicNumber));
     addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new VulnerablePower((AbstractCreature)p, VULN_AMOUNT, false), VULN_AMOUNT));
     addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)p, (AbstractCreature)p, (AbstractPower)new BerserkPower((AbstractCreature)p, VULN_AMOUNT), VULN_AMOUNT));
