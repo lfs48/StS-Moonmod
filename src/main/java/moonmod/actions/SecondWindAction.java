a804d18aeaf192025f8c4bdcae03ebf28a821f9c
Lucas <lucasfschraier@gmail.com>
2022-08-05 22:49:06 -0400

    feat: rework second wind to restore hp

---
+++ src/main/java/moonmod/actions/SecondWindAction.java
@@ -0,0 +1,32 @@
+package moonmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import java.util.ArrayList;
+
+public class SecondWindAction extends AbstractGameAction {
+  private int healPerCard;
+  
+  public SecondWindAction(int healAmount) {
+    this.healPerCard = healAmount;
+    setValues((AbstractCreature)AbstractDungeon.player, (AbstractCreature)AbstractDungeon.player);
+    this.actionType = AbstractGameAction.ActionType.BLOCK;
+  }
+  
+  public void update() {
+    ArrayList<AbstractCard> cardsToExhaust = new ArrayList<>();
+    for (AbstractCard c : AbstractDungeon.player.hand.group) {
+      if (c.type != AbstractCard.CardType.ATTACK)
+        cardsToExhaust.add(c); 
+    } 
+    for (AbstractCard c : cardsToExhaust)
+      addToTop((AbstractGameAction)new HealAction((AbstractCreature)AbstractDungeon.player, (AbstractCreature)AbstractDungeon.player, this.healPerCard)); 
+    for (AbstractCard c : cardsToExhaust)
+      addToTop((AbstractGameAction)new ExhaustSpecificCardAction(c, AbstractDungeon.player.hand)); 
+    this.isDone = true;
+  }
+}
---
+++ src/main/java/moonmod/cards/red/skill/SecondWind.java
@@ -0,0 +1,37 @@
+package moonmod.cards.red.skill;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import moonmod.actions.SecondWindAction;
+import moonmod.cards.BaseCard;
+import moonmod.util.CardInfo;
+
+public class SecondWind extends BaseCard {
+
+    public static final String ID = "Second Wind";
+    public static final int COST = 1;
+    public static final int BASE_MAGIC = 1;
+    public static final int UPG_MAGIC = 1;
+
+    private final static CardInfo cardInfo = new CardInfo(
+        ID, 
+        COST, 
+        CardType.SKILL, 
+        CardTarget.SELF, 
+        CardRarity.UNCOMMON, 
+        CardColor.RED
+    );
+  
+    public SecondWind() {
+        super(cardInfo);
+        this.setMagic(BASE_MAGIC, UPG_MAGIC);
+        this.setExhaust(true);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot((AbstractGameAction)new SecondWindAction(this.magicNumber));
+    }
+
+}
