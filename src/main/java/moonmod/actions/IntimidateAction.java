636689d92a4d7387659bb8bf3ce2542c42d30ed0
Lucas <lucasfschraier@gmail.com>
2022-08-07 15:12:58 -0400

    fix: correct intimidate interaction with artifact

---
+++ src/main/java/moonmod/actions/IntimidateAction.java
@@ -0,0 +1,40 @@
+package moonmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.IntimidateEffect;
+
+public class IntimidateAction extends AbstractGameAction {
+  private AbstractPlayer p = AbstractDungeon.player;
+  private int amount;
+
+  public IntimidateAction(int amount) {
+    this.amount = amount;
+  }
+  
+  public void update() {
+    addToBot((AbstractGameAction)new SFXAction("INTIMIDATE"));
+    addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new IntimidateEffect(p.hb.cX, p.hb.cY), 1.0F));
+    for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+        if (!mo.hasPower("Artifact")) {
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new StrengthPower((AbstractCreature)mo, -this.amount), -this.amount, true, AbstractGameAction.AttackEffect.NONE)); 
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new GainStrengthPower((AbstractCreature)mo, this.amount), this.amount, true, AbstractGameAction.AttackEffect.NONE)); 
+        } else {
+            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new StrengthPower((AbstractCreature)mo, -this.amount), -this.amount, true, AbstractGameAction.AttackEffect.NONE)); 
+        }
+        addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new WeakPower((AbstractCreature)mo, this.amount, false), this.amount, true, AbstractGameAction.AttackEffect.NONE));
+    }
+    tickDuration();
+  }
+}
--- src/main/java/moonmod/cards/red/skill/Intimidate.java
+++ src/main/java/moonmod/cards/red/skill/Intimidate.java
@@ -1,20 +1,10 @@
 package moonmod.cards.red.skill;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.GainStrengthPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
-import com.megacrit.cardcrawl.powers.WeakPower;
-import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
-import com.megacrit.cardcrawl.vfx.combat.IntimidateEffect;
 
+import moonmod.actions.IntimidateAction;
 import moonmod.cards.BaseCard;
 import moonmod.util.CardInfo;
 
@@ -41,14 +31,7 @@ public class Intimidate extends BaseCard {
   }
   
   public void use(AbstractPlayer p, AbstractMonster m) {
-    addToBot((AbstractGameAction)new SFXAction("INTIMIDATE"));
-    addToBot((AbstractGameAction)new VFXAction((AbstractCreature)p, (AbstractGameEffect)new IntimidateEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY), 1.0F));
-    for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
-        addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new WeakPower((AbstractCreature)mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
-        addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new StrengthPower((AbstractCreature)mo, -this.magicNumber), -this.magicNumber, true, AbstractGameAction.AttackEffect.NONE)); 
-        if (!mo.hasPower("Artifact"))
-            addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new GainStrengthPower((AbstractCreature)mo, this.magicNumber), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE)); 
-    }
+    addToBot((AbstractGameAction)new IntimidateAction(this.magicNumber));
   }
 
 }
