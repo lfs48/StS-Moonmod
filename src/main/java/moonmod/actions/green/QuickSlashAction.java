fd7f42ac18908dfaffd729ef93e6617814f3e1d5
Lucas <lucasfschraier@gmail.com>
2022-08-15 21:51:33 -0400

    fix: actually fix quick slash crash

--- src/main/java/moonmod/actions/green/QuickSlashAction.java
+++ src/main/java/moonmod/actions/green/QuickSlashAction.java
@@ -1,21 +1,19 @@
 package moonmod.actions.green;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import moonmod.actions.common.DrawAndDiscountAction;
 public class QuickSlashAction extends AbstractGameAction {
     
     private AbstractPlayer p = AbstractDungeon.player;
   
     public void update() {
+        addToTop((AbstractGameAction)new DrawAndDiscountAction());
         if (p.drawPile.isEmpty())
-            p.drawPile.shuffle();
-        AbstractCard c = p.drawPile.getTopCard();
-        addToTop((AbstractGameAction)new DrawCardAction((AbstractCreature)p, 1));
-        c.setCostForTurn(c.costForTurn - 1);
+            addToTop((AbstractGameAction)new EmptyDeckShuffleAction());
         tickDuration();
     }
 }
