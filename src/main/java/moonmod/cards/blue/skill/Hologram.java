19a9c6ba701448f701e331e6e61d32c30d48caea
Lucas <lucasfschraier@gmail.com>
2022-08-21 10:31:47 -0400

    tweak: increase hologram number of copies

--- src/main/java/moonmod/cards/blue/skill/Hologram.java
+++ src/main/java/moonmod/cards/blue/skill/Hologram.java
@@ -12,7 +12,7 @@ public class Hologram extends BaseCard {
 
     public static final String ID = "Hologram";
     public static final int COST = 0;
-    public static final int BASE_MAGIC = 1;
+    public static final int BASE_MAGIC = 2;
     public static final int UPG_MAGIC = 1;
 
     private final static CardInfo cardInfo = new CardInfo(
