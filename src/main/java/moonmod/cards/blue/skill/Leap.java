17a4fd574a1476b8a548927435c2874aedb98836
Lucas <lucasfschraier@gmail.com>
2022-08-21 10:57:24 -0400

    feat: rework leap

--- src/main/java/moonmod/cards/blue/skill/Leap.java
+++ src/main/java/moonmod/cards/blue/skill/Leap.java
@@ -13,9 +13,9 @@ import moonmod.util.CardInfo;
 public class Leap extends BaseCard {
 
     public static final String ID = "Leap";
-    public static final int COST = 1;
-    public static final int BASE_MAGIC = 7;
-    public static final int UPG_MAGIC = 3;
+    public static final int COST = 0;
+    public static final int BASE_MAGIC = 1;
+    public static final int UPG_MAGIC = 1;
     public static final int BASE_BLOCK = 0;
 
     private final static CardInfo cardInfo = new CardInfo(
@@ -38,7 +38,7 @@ public class Leap extends BaseCard {
     }
 
     public void applyPowers() {
-        this.baseBlock = this.magicNumber + (EnergyPanel.totalCount - this.costForTurn);
+        this.baseBlock = this.magicNumber * (EnergyPanel.totalCount - this.costForTurn);
         super.applyPowers();
         this.rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
         initializeDescription();
